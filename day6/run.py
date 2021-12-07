09c96464511b9890949a84ca66f12dfcb9363e6c
Boushra Kabbash <boushra.kabbash@gmail.com>
2021-12-07 22:26:29 +0100

    DAY06

---
+++ day6/run.py
@@ -0,0 +1,21 @@
+
+myinput= [4,2,4,1,5,1,2,2,4,1,1,2,2,2,4,4,1,2,1,1,4,1,2,1,2,2,2,2,5,2,2,3,1,4,4,4,1,2,3,4,4,5,4,3,5,1,2,5,1,1,5,5,1,4,4,5,1,3,1,4,5,5,5,4,1,2,3,4,2,1,2,1,2,2,1,5,5,1,1,1,1,5,2,2,2,4,2,4,2,4,2,1,2,1,2,4,2,4,1,3,5,5,2,4,4,2,2,2,2,3,3,2,1,1,1,1,4,3,2,5,4,3,5,3,1,5,5,2,4,1,1,2,1,3,5,1,5,3,1,3,1,4,5,1,1,3,2,1,1,1,5,2,1,2,4,2,3,3,2,3,5,1,5,1,2,1,5,2,4,1,2,4,4,1,5,1,1,5,2,2,5,5,3,1,2,2,1,1,4,1,5,4,5,5,2,2,1,1,2,5,4,3,2,2,5,4,2,5,4,4,2,3,1,1,1,5,5,4,5,3,2,5,3,4,5,1,4,1,1,3,4,4,1,1,5,1,4,1,2,1,4,1,1,3,1,5,2,5,1,5,2,5,2,5,4,1,1,4,4,2,3,1,5,2,5,1,5,2,1,1,1,2,1,1,1,4,4,5,4,4,1,4,2,2,2,5,3,2,4,4,5,5,1,1,1,1,3,1,2,1]
+def finding_the_new_zähler_after_1day (input):
+    for i in range(len(input)):
+        age = input[i]
+        age = age -1
+        if age == -1:
+            age = 6
+            input.append(8)
+        input[i] = age
+finding_the_new_zähler_after_1day(myinput)
+print(myinput)
+
+def finding_the_new_zähler_after_yday(days):
+    input1= myinput
+    for y in range(days):
+        print("day", y, "starting with", len(input1), "fishes")
+        finding_the_new_zähler_after_1day(input1)
+    return input1 
+days=256
+finding_the_new_zähler_after_yday(days)
